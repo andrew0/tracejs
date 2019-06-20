90b5b246a7d8853a797166096b9aee8238c49609
andrew0 <739172+andrew0@users.noreply.github.com>
2019-06-19 20:06:22 -0400

    added ability to edit other config values

---
+++ tracejs-vue/src/components/ParameterConfigInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="field is-horizontal" style="width: 30rem;">
+    <div class="field-label">
+      <label class="label" style="width: 10rem;">{{ label }}</label>
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <input class="input" type="text" :value="value" @input="$emit('input', $event.target.value)" />
+        <p class="help is-info">{{ note }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value', 'label', 'note']
+}
+</script>
