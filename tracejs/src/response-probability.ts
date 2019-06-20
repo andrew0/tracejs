5d2e15db3f14bc64bd6b575133bc6cc5d6ad0598
andrew0 <739172+andrew0@users.noreply.github.com>
2019-06-19 20:05:55 -0400

    fixed response probability code

--- tracejs/src/response-probability.ts
+++ tracejs/src/response-probability.ts
@@ -10,16 +10,16 @@ import { RdlNode } from './trace-param'
 
 export const applyRestScaling = (node: RdlNode, num: number): number => {
   if (node.RDL_rest_s) {
-    return node.RDL_rest_s * Math.log10(this.RDL_rest_c * num)
+    return node.RDL_rest_s * Math.log10(node.RDL_rest_c * num)
   }
   return 0.0
 }
 
 export const applyWeightScaling = (node: RdlNode, num: number, act_pi: number): number => {
-  if (this.RDL_wt_s) {
+  if (node.RDL_wt_s) {
     //TODO!
     //NEW VERSION BASED ON JIM'S CODE:
-    return act_pi * (1.0 + this.RDL_wt_s * Math.log10(num))
+    return act_pi * (1.0 + node.RDL_wt_s * Math.log10(num))
     //HOW I ORIGINALLY DID IT, BASED ON THE PAPER:
     //double result=(act_pi * (1.0d + (act_pi * RDL_wt_s * (Math.log(RDL_wt_c + word_i.getFrequency()) * 0.434294482))));
   }
@@ -27,8 +27,8 @@ export const applyWeightScaling = (node: RdlNode, num: number, act_pi: number): 
 }
 
 export const applyPostActivationScaling = (node: RdlNode, num: number, exp_act_i: number) => {
-  if (this.RDL_post_c) {
-    return exp_act_i * Math.log10(this.RDL_post_c + num)
+  if (node.RDL_post_c) {
+    return exp_act_i * Math.log10(node.RDL_post_c + num)
   }
   return 0.0
 }
