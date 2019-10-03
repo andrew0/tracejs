2360da450c2d090241ff6dde6b4df520b35cb645
andrew0 <739172+andrew0@users.noreply.github.com>
2019-10-03 18:13:40 -0400

    added start/stop button to toolbar

--- tracejs-vue/src/components/SimulationToolbar.vue
+++ tracejs-vue/src/components/SimulationToolbar.vue
@@ -6,11 +6,12 @@
       cycle:
       <a class="button" @click="$emit('update:cycle', Math.min(Math.max(cycle - 1, 0), numCycles - 1))">-</a>
       <div class="select">
-        <select :value="cycle" @change="$emit('update:cycle', $event.target.value)">
+        <select :value="cycle" @change="$emit('update:cycle', Number($event.target.value))">
           <option v-for="(n, index) in numCycles" :key="index" :value="index">{{ index }}</option>
         </select>
       </div>
       <a class="button" @click="$emit('update:cycle', Math.min(Math.max(cycle + 1, 0), numCycles - 1))">+</a>
+      <a style="margin-left: 1rem;" class="button" @click="toggleTimer">{{ timer == null ? 'start' : 'stop' }}</a>
     </span>
 
     <span v-if="showChartOptions" style="margin-left: 2rem;">
@@ -30,6 +31,30 @@ export default {
     showCycles: Boolean,
     showChartOptions: Boolean,
     visualize: Boolean
+  },
+  data() {
+    return {
+      timer: null
+    }
+  },
+  methods: {
+    toggleTimer() {
+      if (this.timer == null) {
+        const fn = () => {
+          const newVal = Math.min(Math.max(this.cycle + 1, 0), this.numCycles - 1)
+          this.$emit('update:cycle', newVal)
+          if (newVal == this.numCycles - 1) {
+            clearInterval(this.timer)
+            this.timer = null
+          }
+        }
+        this.timer = setInterval(fn, 250)
+        fn()
+      } else {
+        clearInterval(this.timer)
+        this.timer = null
+      }
+    }
   }
 }
 </script>
