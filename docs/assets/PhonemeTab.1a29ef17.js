a03c008f1afbb6248cbb6aa25583a335808326ae
andrew0 <739172+andrew0@users.noreply.github.com>
2021-02-24 16:59:02 -0500

    update build

---
+++ docs/assets/PhonemeTab.1a29ef17.js
@@ -0,0 +1,1 @@
+import{d as a,g as t,o,c as e,r}from"./index.fea91626.js";import{_ as n}from"./DataTab.28830d5c.js";import"./SimulationToolbar.da487e40.js";var s=a({name:"PhonemeTab",components:{DataTab:n},setup:()=>({data:t().formattedPhonemeData})});s.render=function(a,t,n,s,d,m){const i=r("DataTab");return o(),e(i,{data:a.data},null,8,["data"])};export default s;
