263d680978c32d7a32728626838750a42067f1e1
andrew0 <739172+andrew0@users.noreply.github.com>
2021-04-19 21:34:38 -0400

    fix playground code

--- packages/playground/index.js
+++ packages/playground/index.js
@@ -10,8 +10,8 @@ async function simulate() {
 
   // Create file handles that we can continuously write to over the course of the
   // simulation
-  const featureFile = tracjs.openFileHandle('./playground-sim-feature.csv.gz');
-  const phonemeFile = tracejs.openFileHandles('./playground-phoneme.csv.gz');
+  const featureFile = tracejs.openFileHandle('./playground-sim-feature.csv.gz');
+  const phonemeFile = tracejs.openFileHandle('./playground-phoneme.csv.gz');
   const wordFile = tracejs.openFileHandle('./playground-sim-word.csv.gz');
   const levelsAndFlowFile = tracejs.openFileHandle('./playground-sim-levels-and-flow.csv.gz');
 
