d1b0f2eae7d096812c3cf5d35844543924f5d15b
andrew0 <739172+andrew0@users.noreply.github.com>
2021-07-18 23:02:44 -0400

    add basic regression test

---
+++ packages/tracejs/src/__tests__/regression.test.ts
@@ -0,0 +1,12 @@
+import {createDefaultConfig, TraceSim} from '..';
+
+test('output does not change unexpectedly', () => {
+  const config = createDefaultConfig();
+  const sim = new TraceSim(config);
+  sim.cycle(15);
+  expect(sim.getAllInputData()).toMatchSnapshot();
+  expect(sim.getAllFeatureData()).toMatchSnapshot();
+  expect(sim.getAllPhonemeData()).toMatchSnapshot();
+  expect(sim.getAllWordData()).toMatchSnapshot();
+  expect(sim.getAllLevelsAndFlowData()).toMatchSnapshot();
+});
