0dbf42ccdc99209533d2487a0ed6656a69267697
andrew0 <739172+andrew0@users.noreply.github.com>
2022-02-09 22:49:18 -0500

    fix: fix response probability

--- packages/tracejs/src/response-probability.ts
+++ packages/tracejs/src/response-probability.ts
@@ -16,11 +16,7 @@ export const applyRestScaling = (node: RdlNode, num: number): number => {
 
 export const applyWeightScaling = (node: RdlNode, num: number, act_pi: number): number => {
   if (node.RDL_wt_s) {
-    //TODO!
-    //NEW VERSION BASED ON JIM'S CODE:
-    return act_pi * (1.0 + node.RDL_wt_s * Math.log10(num));
-    //HOW I ORIGINALLY DID IT, BASED ON THE PAPER:
-    //double result=(act_pi * (1.0d + (act_pi * RDL_wt_s * (Math.log(RDL_wt_c + word_i.getFrequency()) * 0.434294482))));
+    return act_pi * (1.0 + act_pi * node.RDL_wt_s * Math.log10(1.0 + num));
   }
   return act_pi;
 };
